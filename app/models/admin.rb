058a96088958f598651194b19cda1e09cbad84ca
Bruno Almeida <bruno@disvolvi.com>
2014-08-20 19:52:46 -0300

    password_digest and admin_params

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,14 @@
 class Admin < ActiveRecord::Base
+	
+  def create
+    Admin.create(admim_params)
+  end
+
+  has_secure_password
+
+  private
+
+  def admim_params
+    params.require(:admin).permit(:name, :login, :password, :password_confirmation)
+  end
 end
